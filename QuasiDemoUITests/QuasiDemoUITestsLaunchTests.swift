d76570f5cee87d72ff5e42e4d456084bcbf53393
Soul <soulstayreal@gmail.com>
2022-03-08 17:34:02 +0800

    Init - demo for Quasi SDK

---
+++ QuasiDemoUITests/QuasiDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QuasiDemoUITestsLaunchTests.swift
+//  QuasiDemoUITests
+//
+//  Created by GengJian on 2022/3/8.
+//
+
+import XCTest
+
+class QuasiDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
